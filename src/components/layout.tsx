1d29788490f426e5144f7aeceae34b183a7f6193
zaid-bk <zaid@maxenius.agency>
2021-09-02 17:00:51 +0500

    impliment scss

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import Helmet from "react-helmet"
 import Header from "./header/Header"
 import Footer from "./footer/Footer"
-import "../styles/custom.css"
+import "../styles/app.scss"
 import "react-multi-carousel/lib/styles.css"
 
 const Layout = ({ children }: any) => {
