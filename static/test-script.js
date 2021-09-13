18f94a4f040c4942feaf9f116d7cd474298833e3
zaid-bk <zaid@maxenius.agency>
2021-09-13 16:52:44 +0500

    change in smartlook integration

--- static/test-script.js
+++ static/test-script.js
@@ -19,3 +19,23 @@ smartlook("init", "5d9e8bf308ff17eb5749d15dccc8bf1271ac2e37")
 {
   /* </script> */
 }
+
+{
+  /* <script type='text/javascript'> */
+}
+window.smartlook ||
+  (function (d) {
+    var o = (smartlook = function () {
+        o.api.push(arguments)
+      }),
+      h = d.getElementsByTagName("head")[0]
+    var c = d.createElement("script")
+    o.api = new Array()
+    c.async = true
+    c.type = "text/javascript"
+    c.charset = "utf-8"
+    c.src = "https://rec.smartlook.com/recorder.js"
+    h.appendChild(c)
+  })(document)
+smartlook("init", "2043581d9aaf1d21018a21f77d2693821f3ae976")
+// </script>
