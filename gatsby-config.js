f3179a2be0a252fb420e69d031dc5e109552d167
zaid-bk <zaid@maxenius.agency>
2021-09-13 11:12:01 +0500

    trying smartlook

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,12 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-load-script",
+      options: {
+        src: "/test-script.js", // Change to the script filename
+      },
+    },
+    {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
