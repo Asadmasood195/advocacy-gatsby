0de0695b2db4bc85f265b4860e43c9a4fcc71776
usama-govsoft <robert@govsoft.us>
2021-09-17 15:48:31 +0500

    cart table

--- src/components/cart/Products.tsx
+++ src/components/cart/Products.tsx
@@ -7,16 +7,16 @@ const Products = () => {
 
     const products = [
         {
-            'id':1,
-            'img':Image,
+            'id': 1,
+            'img': Image,
             'title': '2021 Pocket Directory of the California Legislature',
             'note': 'Quantity discounts available',
             'price': 17.95,
             'unit_price': 35.25,
         },
         {
-            'id':2,
-            'img':Image,
+            'id': 2,
+            'img': Image,
             'title': '2020 Pocket Directory of the California Legislature',
             'note': 'Quantity discounts available',
             'price': 17.95,
@@ -25,55 +25,55 @@ const Products = () => {
     ];
 
     return (
-        <>
-            <table>
+        <div style={{ overflowY: 'scroll' }}>
+            <table >
                 <thead>
-                <tr>
-                    <th></th>
-                    <th colSpan={2}>PRODUCT</th>
-                    <th>PRICE</th>
-                    <th>QTY</th>
-                    <th>UNIT PRICE</th>
-                </tr>
+                    <tr>
+                        <th></th>
+                        <th colSpan={2}>PRODUCT</th>
+                        <th>PRICE</th>
+                        <th>QTY</th>
+                        <th>UNIT PRICE</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {products.map((product) => {
-                    return(                    
-                    <tr key={product.id}>
-                        <td>
-                        <a href="#" className="btn-delete">
-                            <X16 className="svg" />
-                        </a>
-                        </td>
-                        <td className="product-image">
-                        <img src={product.img} alt="sample" />
-                        </td>
-                        <td>
-                        <div className="product-title">
-                            {product.title}
-                        </div>
-                        <div className="note1">
-                            {product.note}
-                        </div>
-                        </td>
-                        <td>
-                        <div className="price">{product.price}</div>
-                        </td>
-                        <td>
-                        <div className="qty">
-                            <CartInput />
-                        </div>
-                        </td>
-                        <td>
-                        <div className="unit-price">{product.unit_price}</div>
-                        </td>
-                    </tr>
-                    )
-                })}
-                
+                    {products.map((product) => {
+                        return (
+                            <tr key={product.id}>
+                                <td>
+                                    <a href="#" className="btn-delete">
+                                        <X16 className="svg" />
+                                    </a>
+                                </td>
+                                <td className="product-image">
+                                    <img src={product.img} alt="sample" />
+                                </td>
+                                <td>
+                                    <div className="product-title">
+                                        {product.title}
+                                    </div>
+                                    <div className="note1">
+                                        {product.note}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="price">{product.price}</div>
+                                </td>
+                                <td>
+                                    <div className="qty">
+                                        <CartInput />
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="unit-price">{product.unit_price}</div>
+                                </td>
+                            </tr>
+                        )
+                    })}
+
                 </tbody>
             </table>
-        </>
+        </div>
     )
 }
 
